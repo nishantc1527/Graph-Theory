45d6c460ecdbba33057798cf955c03ca96e6a71c
nishantc1527 <52992695+nishantc1527@users.noreply.github.com>
2020-04-05 18:33:26 -0700

    Update GraphTester.py

--- GraphTester.py
+++ GraphTester.py
@@ -18,14 +18,30 @@ graph.connect(3, 4, 0)  # connects vertex 4 to 3
 graph.connect(4, 5, 0)  # connects vertex 5 to 4
 graph.connect(1, 5, 0)  # connects vertex 1 to 5
 graph.connect(5, 2, 0)  # connects vertex 2 to 5
-print_shortest_path_bfs(graph, 1, 2)
+print_in_bfs(graph, 1)
 
-# 1 -> 2: 6
-# 2 -> 3: 5
-# 3 -> 4: 2
-# 4 -> 5: 3
-# 1 -> 5: 1
-# 5 -> 2: 1
+# 1 -> 2 -> 3
+# ↓  ↑      ↑
+# 5 <- 4 ->
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+graph = DirectedGraph()  # prints the shortest path from source to target in a directed graph using
+# breadth-first-search algorithm
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+graph.add_vertex(5)
+graph.connect(1, 2, 0)  # connects vertex 1 to 2
+graph.connect(2, 3, 0)  # connects vertex 2 to 3
+graph.connect(3, 4, 0)  # connects vertex 4 to 3
+graph.connect(4, 5, 0)  # connects vertex 5 to 4
+graph.connect(1, 5, 0)  # connects vertex 1 to 5
+graph.connect(5, 2, 0)  # connects vertex 2 to 5
+print_shortest_path_bfs(graph, 1, 2)
 
 # 1 -> 2 -> 3
 # ↓  ↑      ↑
