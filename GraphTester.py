7caecae07be19118b4d7064ba587f253bb2e9f2f
nishantc1527 <52992695+nishantc1527@users.noreply.github.com>
2020-04-05 17:49:57 -0700

    Add files via upload

---
+++ GraphTester.py
@@ -0,0 +1,88 @@
+# --------------------------------------------------------
+# This file is used to test all the algorithms performed
+# on the graph's
+# --------------------------------------------------------
+
+from Graph import *
+
+graph = DirectedGraph()  # prints the shortest path from source to target in a directed graph using
+# breadth-first-search algorithm
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+graph.add_vertex(5)
+graph.connect(1, 2, 0)  # connects vertex 1 to 2
+graph.connect(2, 3, 0)  # connects vertex 2 to 3
+graph.connect(3, 4, 0)  # connects vertex 4 to 3
+graph.connect(4, 5, 0)  # connects vertex 5 to 4
+graph.connect(1, 5, 0)  # connects vertex 1 to 5
+graph.connect(5, 2, 0)  # connects vertex 2 to 5
+print_shortest_path_bfs(graph, 1, 2)
+
+# 1 -> 2: 6
+# 2 -> 3: 5
+# 3 -> 4: 2
+# 4 -> 5: 3
+# 1 -> 5: 1
+# 5 -> 2: 1
+
+# 1 -> 2 -> 3
+# ↓  ↑      ↑
+# 5 <- 4 ->
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+graph = WeightedGraph()  # prints the shortest path from source to target in a weighted graph using dikstra algorithm
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+graph.add_vertex(5)
+graph.connect(1, 2, 6)  # connects vertex 1 to 2 with weight of 6
+graph.connect(2, 3, 5)  # connects vertex 2 to 3 with weight of 5
+graph.connect(3, 4, 2)  # connects vertex 4 to 3 with weight of 2
+graph.connect(4, 5, 3)  # connects vertex 5 to 4 with weight of 3
+graph.connect(1, 5, 1)  # connects vertex 1 to 5 with weight of 1
+graph.connect(5, 2, 1)  # connects vertex 2 to 5 with weight of 1
+print_shortest_path_dikstra(graph, 1, 2)
+
+# 1 -> 2: 6
+# 2 -> 3: 5
+# 3 -> 4: 2
+# 4 -> 5: 3
+# 1 -> 5: 1
+# 5 -> 2: 1
+
+# 1 -> 2 -> 3
+# ↓  ↑      ↑
+# 5 <- 4 ->
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+graph = WeightedGraph()  # prints the shortest path from source to target in a weighted graph using bellman ford
+# algorithm
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+graph.add_vertex(5)
+graph.connect(1, 2, 6)  # connects vertex 1 to 2 with weight of 6
+graph.connect(2, 3, 5)  # connects vertex 2 to 3 with weight of 5
+graph.connect(3, 4, 2)  # connects vertex 4 to 3 with weight of 2
+graph.connect(4, 5, 3)  # connects vertex 5 to 4 with weight of 3
+graph.connect(1, 5, 1)  # connects vertex 1 to 5 with weight of 1
+graph.connect(5, 2, 1)  # connects vertex 2 to 5 with weight of 1
+print_shortest_path_bellman_ford(graph, 1, 2)
+
+# 1 -> 2: 6
+# 2 -> 3: 5
+# 3 -> 4: 2
+# 4 -> 5: 3
+# 1 -> 5: 1
+# 5 -> 2: 1
+
+# 1 -> 2 -> 3
+# ↓  ↑      ↑
+# 5 <- 4 ->
